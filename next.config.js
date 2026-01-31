5e83eb262ac85f2f8f644c187808b3d683ca632e
donwheeler <slowheelerfam@gmail.com>
2026-01-31 12:21:45 -0800

    Fix build: remove API routes for frontend-only deploy

--- next.config.js
+++ next.config.js
@@ -1,40 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: __dirname,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'static.nike.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.stockx.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.goat.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'stockx.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'stockx.imgix.net',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/**',
-      },
-    ],
-  },
+  output: 'export',
 };
 
 module.exports = nextConfig;
